d61439389e1ad678d232e850e2aa1b94f1099cf9
Lilicorn <putyo0302@gmail.com>
2026-09-06 17:28:27 +0200

    fixed ligh/dark mode button issues, added tooltips; and a TON of animations. might be too much actually

--- display.js
+++ display.js
@@ -53,21 +53,25 @@ window.onload = function() {
 
     document.getElementById("displaymode").addEventListener('click', displaymode)
     if(document.getElementsByClassName("dark").length===0){
-        document.getElementById("displaymode").innerHTML="🌙";
+        document.getElementById("modeicon").textContent="🌙";
+        document.getElementById("displayhelper").innerHTML="Switch to light mode";
     }
     else{
-        document.getElementById("displaymode").innerHTML="☀️";
+        document.getElementById("modeicon").textContent="☀️";
+         document.getElementById("displayhelper").innerHTML="Switch to dark mode";
     }
 }
 
 function displaymode(){
     var element = document.body;
     element.classList.toggle("dark");
-    console.log(document.getElementsByClassName("dark"));
+    //console.log(document.getElementsByClassName("dark"));
     if(document.getElementsByClassName("dark").length===0){
-        document.getElementById("displaymode").innerHTML="🌙";
+        document.getElementById("modeicon").textContent="🌙";
+        document.getElementById("displayhelper").innerHTML="Switch to light mode";
     }
     else{
-        document.getElementById("displaymode").innerHTML="☀️";
+        document.getElementById("modeicon").textContent="☀️";
+         document.getElementById("displayhelper").innerHTML="Switch to dark mode";
     }
 }
