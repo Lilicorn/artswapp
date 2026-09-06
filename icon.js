0aca858e3b4cae57ae13a21e9eea28ac0cd9bb11
Lilicorn <putyo0302@gmail.com>
2026-09-06 11:09:25 +0200

    the important things

---
+++ icon.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+    let currentTitle = document.title;
+
+    //a weblaphoz animáció
+    /*const frames = [
+        "icon/0.png", "icon/2.png", "icon/4.png", "icon/6.png", 
+        "icon/8.png", "icon/10.png"
+    ];*/
+    const titles = [
+        " ₊ ⊹ ⋆ .˚⋆ ",
+        " ⊹ ⋆ .˚⋆ ₊ ",
+        " ༝ ⋆ .˚⋆ ₊ ",
+        " ⋆ .˚⋆ ₊ ⊹ ",
+        " .˚⋆ ₊ ⊹ ⋆ ",
+        " ˚⋆ ₊ ⊹ ⋆ ."
+    ];
+    
+    const intro = [
+        "ㅤㅤㅤㅤ",
+        "ㅤㅤㅤㅤ"+ ".",
+        "ㅤㅤㅤ"+ " .˚ ",
+        "ㅤㅤ"+ " .˚⋆ ",
+        "ㅤ"+ " .˚⋆ ₊ ",
+        " .˚⋆ ₊ ⊹ ⋆ ",
+        "˚⋆ ₊ ⊹ ⋆ ."
+    ]
+    const flicker=["♡︎⁠ ","♡︎⁠ ", "♥︎ ","♥︎ "]
+
+    //let i = 0; 
+    let j = 1;
+    let l = 0;
+    for (let k = 0; k < intro.length; k++) {
+        setTimeout(() => {
+            document.title = "♥︎ " + currentTitle +" "+ intro[k];
+        }, k * 300);
+    }
+
+    setTimeout(() => {
+        setInterval(updateFavicon, 300);
+    }, intro.length * 300);
+
+    function updateFavicon() {
+        /*let favicon = document.getElementById("icon"); 
+        if (favicon) {
+            favicon.href = frames[i]; 
+            i = (i + 1) % frames.length; 
+        }*/
+       //nem lenne rossz csak szerintem sok memoriat veszunk el mar igy is x
+        document.title = flicker[l]+currentTitle +" "+ titles[j];
+        j = (j + 1) % titles.length;
+        l = (l + 1) % flicker.length;
+    }
+
+}
