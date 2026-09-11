6626ea502b2c147b8e4f5a8c2c531c36e88ba941
Lilicorn <putyo0302@gmail.com>
2026-09-11 11:04:43 +0200

    card shenanigans

--- home.js
+++ home.js
@@ -2,8 +2,17 @@ function generatecards (mainID, elementnum){
     let main = document.getElementById(mainID);
     main.innerHTML="";
     for(let i = 0; i<elementnum; i++){
-        let card = document.createElement("div");
+        let card = document.createElement("section");
         card.className="card";
+        let image = document.createElement("div");
+        image.className="cardimage";
+        let caption = document.createElement("div");
+        caption.className="cardcaption";
+        let pfp = document.createElement("div");
+        pfp.className="cardpfp";
+        card.appendChild(image);
+        card.appendChild(caption);
+        caption.appendChild(pfp);
         main.appendChild(card);
     }
     // termeszetesen ezt majd tovabb kell 
@@ -16,7 +25,7 @@ function loadingstuff(){
     let en = window.innerWidth <= 768 ? 5 : 10;
     generatecards("recommendedusers", en);
     generatecards("recommendedauctions", en);
-    generatecards("donatorleaderboard", 5);
+    generatecards("donatorleaderboard", en);
 }
 
 window.addEventListener("load", loadingstuff);
