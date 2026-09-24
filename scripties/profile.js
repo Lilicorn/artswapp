6e06277e883b39fa428df10059e623ae2b715fb7
Lilicorn <putyo0302@gmail.com>
2026-09-24 15:27:35 +0200

    layout done!!!!!!!! i think

--- scripties/profile.js
+++ scripties/profile.js
@@ -2,3 +2,15 @@
 window.addEventListener("resize", function() {
     document.getElementsByClassName("swapslots")[0].innerHTML = window.innerWidth <= 380 ? "0/5" : "0/5 swaps";
 });
+
+document.getElementById("seemorebio").addEventListener("click", function(){
+    document.getElementsByClassName("bio")[0].classList.toggle("open");
+});
+
+document.getElementById("seemoreportfolio").addEventListener("click", function(){
+    document.getElementsByClassName("portfolio")[0].classList.toggle("open");
+});
+
+document.getElementById("seemoreswaps").addEventListener("click", function(){
+    document.getElementsByClassName("swaps")[0].classList.toggle("open");
+});
